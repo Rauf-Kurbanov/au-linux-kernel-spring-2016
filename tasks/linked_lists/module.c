370275c1ba8728d24512eae7aefd06d1a5eb9f30
rauf_kurbanov <kurbanov.re@gmail.com>
2016-03-09 22:03:48 +0300

    ha_2 ready

--- tasks/linked_lists/module.c
+++ tasks/linked_lists/module.c
@@ -7,19 +7,19 @@
 #include "stack.h"
 #include "assert.h"
 
-static void __init test_stack(void)
+static int __init test_stack(void)
 {
     LIST_HEAD(data_stack);
     stack_entry_t *tos = NULL;
     const char *tos_data = NULL;
     const char* test_data[] = { "1", "2", "3", "4" };
-    unsigned long i = 0;
+    long i = 0;
 
     pr_alert("Testing basic stack");
 
     for (i = 0; i != ARRAY_SIZE(test_data); ++i) {
         stack_push(&data_stack,
-            create_stack_entry((void*)test_data[i])
+                   create_stack_entry((void*)test_data[i])
         );
     }
 
@@ -32,19 +32,48 @@ static void __init test_stack(void)
     }
 
     assert(stack_empty(&data_stack));
+    return 0;
 }
 
-static void __init print_processes_backwards(void)
+static int __init print_processes_backwards(void)
 {
-    // TODO
+    int code = 0;
+    struct task_struct *tsk;
+    LIST_HEAD(log_stack);
+    stack_entry_t *se = NULL;
+    for_each_process(tsk) {
+        char *buf = kmalloc(sizeof(tsk->comm), GFP_KERNEL);
+        buf = get_task_comm(buf, tsk);
+        if (!buf) {
+            code = -ENOMEM;
+            break;
+        }
+        se = create_stack_entry((void*)buf);
+        if (!se) {
+            code = - ENOMEM;
+            break;
+        }
+        stack_push(&log_stack, se);
+    }
+    while (!stack_empty(&log_stack))
+    {
+        se = stack_pop(&log_stack);
+        const char *buf = STACK_ENTRY_DATA(se, const char*);
+        delete_stack_entry(se);
+        printk(KERN_ALERT "%s\n", buf);
+        kfree(buf);
+    }
+    return code;
 }
 
 static int __init ll_init(void)
 {
     printk(KERN_ALERT "Hello, linked_lists\n");
-    test_stack();
-    print_processes_backwards();
-    return 0;
+    int code = 0;
+    code = test_stack();
+    if (!code)
+        code = print_processes_backwards();
+    return code;
 }
 
 static void __exit ll_exit(void)
--- tasks/linked_lists/stack.c
+++ tasks/linked_lists/stack.c
@@ -1,28 +1,31 @@
+#include <linux/slab.h>
+#include <linux/gfp.h>
+
 #include "stack.h"
 
 stack_entry_t* create_stack_entry(void *data)
 {
-    // TODO
-    (void)data;
-    return NULL;
+    stack_entry_t* se = kmalloc(sizeof(stack_entry_t), GFP_KERNEL);
+    if (se)
+        se->data = data;
+    return se;
 }
 
 void delete_stack_entry(stack_entry_t *entry)
 {
-    // TODO
-    (void)entry;
+    kfree(entry);
 }
 
 void stack_push(struct list_head *stack, stack_entry_t *entry)
 {
-    // TODO
-    (void)stack;
-    (void)entry;
+    list_add(&entry->lh, stack);
 }
 
 stack_entry_t* stack_pop(struct list_head *stack)
 {
-    // TODO
-    (void)stack;
-    return NULL;
+    if (stack_empty(stack))
+        return NULL;
+    stack_entry_t* se = list_first_entry(stack, stack_entry_t, lh);
+    list_del(stack->next);
+    return se;
 }
